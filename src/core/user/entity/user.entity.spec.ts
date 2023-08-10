299f6d885e1d215e29359f48b37670e555b612b9
mori5321 <moriwm77@gmail.com>
2023-08-10 17:29:05 +0900

    feat: impl user domain

---
+++ src/core/user/entity/user.entity.spec.ts
@@ -0,0 +1,67 @@
+import { mkUser } from './user.entity';
+import { v4 as genUUID } from 'uuid';
+import { right, left } from 'fp-ts/lib/Either';
+
+describe('mkUser', () => {
+  const userId = genUUID();
+
+  it('should return a valid user', () => {
+    const user = mkUser(userId, 'John Doe');
+    expect(user).toEqual(right({ id: userId, name: 'John Doe' }));
+  });
+
+  it('should return left with empty user name', () => {
+    const user = mkUser(userId, '');
+    expect(user).toEqual(
+      left([
+        {
+          message: 'User name must not be empty',
+          __tag: 'InvalidUserNameError',
+        },
+      ]),
+    );
+  });
+
+  it('should return left with empty user name', () => {
+    const user = mkUser(userId, 'more than 16 characters');
+    expect(user).toEqual(
+      left([
+        {
+          message: 'User name must be less than 16 characters',
+          __tag: 'InvalidUserNameError',
+        },
+      ]),
+    );
+  });
+
+  it('should return left with invalid id(= 32 chars)', () => {
+    const user = mkUser(
+      'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+      'John Doe',
+    );
+    expect(user).toEqual(
+      left([
+        {
+          message: 'User id must be UUID v4(32 characters)',
+          __tag: 'InvalidUserIdError',
+        },
+      ]),
+    );
+  });
+
+  it('can return multiple errors', () => {
+    const user = mkUser('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', '');
+    expect(user).toEqual(
+      left([
+        {
+          message: 'User id must be UUID v4(32 characters)',
+          __tag: 'InvalidUserIdError',
+        },
+        {
+          message: 'User name must not be empty',
+          __tag: 'InvalidUserNameError',
+        },
+      ]),
+    );
+  });
+});
--- src/core/user/entity/user.entity.ts
+++ src/core/user/entity/user.entity.ts
@@ -1,23 +1,79 @@
+import { getSemigroup } from 'fp-ts/lib/Array';
 import { Flavor } from '../../../utils/flavor';
+import * as E from 'fp-ts/lib/Either';
+import { Either, right, left } from 'fp-ts/lib/Either';
+import { pipe } from 'fp-ts/lib/function';
+import { sequenceS } from 'fp-ts/lib/Apply';
+
+type Validation<E, A> = E.Either<E[], A>;
+const lift: <E, A>(body: Either<E, A>) => Validation<E, A> = (body) =>
+  pipe(
+    body,
+    E.mapLeft((a) => [a]),
+  );
+
+const getValidation = <T>() => E.getApplicativeValidation(getSemigroup<T>());
 
 type UserId = Flavor<string, 'UserId'>;
 type UserName = Flavor<string, 'UserName'>;
 
-const mkUserId = (id: string): UserId => {
-  // Validate
-  return id as UserId;
+type Error<K extends string> = Flavor<
+  {
+    message: string;
+  },
+  K
+>;
+
+type InvalidUserIdError = Error<'InvalidUserIdError'>;
+const InvalidUserIdError = {
+  new: (message: string): InvalidUserIdError => ({
+    message,
+    __tag: 'InvalidUserIdError',
+  }),
 };
 
-const mkUserName = (name: string): UserName => {
-  // Validat
-  return name as UserName;
+type InvalidUserNameError = Error<'InvalidUserNameError'>;
+const InvalidUserNameError = {
+  new: (message: string): InvalidUserNameError => ({
+    message,
+    __tag: 'InvalidUserNameError',
+  }),
 };
 
-export const mkUser = (id: string, name: string): User => {
-  return {
-    id: mkUserId(id),
-    name: mkUserName(name),
-  };
+type UserError = InvalidUserIdError | InvalidUserNameError;
+
+const mkUserId = (id: string): Either<InvalidUserIdError, UserId> => {
+  if (id.length !== 36) {
+    const err = InvalidUserIdError.new(
+      `User id must be UUID v4(32 characters)`,
+    );
+    return left(err);
+  }
+
+  return right(id as UserId);
+};
+
+const mkUserName = (name: string): Either<InvalidUserNameError, UserName> => {
+  if (name.length > 16) {
+    const err = InvalidUserNameError.new(
+      `User name must be less than 16 characters`,
+    );
+    return left(err);
+  }
+
+  if (name.length === 0) {
+    const err = InvalidUserNameError.new(`User name must not be empty`);
+    return left(err);
+  }
+
+  return right(name as UserName);
+};
+
+export const mkUser = (id: string, name: string): Either<UserError[], User> => {
+  return sequenceS(getValidation<UserError>())({
+    id: lift(mkUserId(id)),
+    name: lift(mkUserName(name)),
+  });
 };
 
 export type User = {
