58341e4f76b2e06b1b91b47ffcb5797af8e1aa8d
mori5321 <moriwm77@gmail.com>
2023-08-10 14:07:46 +0900

    init core module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ExpenseModule } from './expense/expense.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ExpenseModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
